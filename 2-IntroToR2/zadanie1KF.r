62c3869b558be0dae3b2e84c657509dfb8c45c06
7idos <7idoskat@gmail.com>
2024-10-15 10:58:34 +0200

    Add files via upload

---
+++ 2-IntroToR2/zadanie1KF.r
@@ -0,0 +1,26 @@
+## Zadanie 1: Zapoznanie się z RStudio
+
+**Cel:** Poznanie interfejsu i podstawowych funkcji RStudio.
+
+### Instrukcje i rozwiązanie:
+
+1.  **Uruchom RStudio** na swoim komputerze.
+
+2.  **Zapoznaj się z interfejsem użytkownika:**
+  
+  3.  **Stwórz nowy skrypt R:**
+  
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+#konsola - służy do wykonywania skryptów
+#środowisko - wszystko co zostało przypisane pojawia się w środowisku
+#źródło(edytor skryptu) - służy do pisania skryptów a funkcje nie są automatycznie wykonywane
+#panel wyjściowy - rzeczy ułatwiają prace w R-ce
+
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+#Dracula - bo dracula i jest różowy <3
+
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
